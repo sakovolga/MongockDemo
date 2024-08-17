29d509165a9dac166409a15678378d296c68e7a5
sakovolga <sakovolga@live.com>
2024-08-17 07:32:55 +0200

    add toString

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -36,4 +36,13 @@ public class User {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
